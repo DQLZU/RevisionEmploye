6b8b4b112495eb93f2196eed3c9c762b28c4a154
DQLZU <109611842+DQLZU@users.noreply.github.com>
2025-03-07 08:15:08 -0500

    Add project files.

---
+++ Employe.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RevisionEmploye
+{
+    public class Employe
+    {
+        private string _matricule;
+        private string _nom;
+        private string _prenom;
+        private DateTime _dateNaissance;
+        private decimal _salaireAnnuel;
+        private DateTime _entreeEnFonction;
+
+        public string Matricule
+        {
+            get => _matricule;
+            set
+            {
+                if (value.Length == 7 || int.TryParse(value, out _))
+                {
+                     _matricule = value;
+                }
+                else 
+                { 
+                    throw new FormatException("Le matricule doit être composé de sept chiffres.");
+                }  
+                
+            }
+        }
+
+        public string Nom
+        {
+            get => _nom;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Le nom ne peut pas être vide.");
+                _nom = value.Trim();
+            }
+        }
+
+        public string Prenom
+        {
+            get => _prenom;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Le prénom ne peut pas être vide.");
+                _prenom = value.Trim();
+            }
+        }
+
+        public DateTime DateNaissance
+        {
+            get => _dateNaissance;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("La date de naissance ne peut pas être dans le futur.");
+                }
+                else
+                {
+                    _dateNaissance = value;
+                }
+            }
+        }
+
+        public double SalaireAnnuel
+        {
+            get => (double)_salaireAnnuel;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Le salaire annuel ne peut pas être négatif.");
+                _salaireAnnuel = (decimal)Math.Round(value, 2);
+            }
+        }
+
+        public DateTime EntreeEnFonction
+        {
+            get => _entreeEnFonction;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("La date d'entrée ne peut pas être dans le futur.");
+                _entreeEnFonction = value;
+            }
+        }
+
+
+        public Employe(string matricule, string nom, string prenom, DateTime dn, double salaire, DateTime ef) 
+        {
+            Matricule = matricule;
+            Nom = nom;
+            Prenom = prenom;
+            DateNaissance = dn;
+            SalaireAnnuel = salaire;
+            EntreeEnFonction = ef;
+        }
+
+        public int Age() 
+        {
+            DateTime dateActuelle = DateTime.Now;
+            TimeSpan age = dateActuelle - DateNaissance;
+
+            int total= (int)Math.Truncate(age.TotalDays / 365.025); 
+
+            return total;
+        }
+
+        public int Anciennete() 
+        {
+            int anciennete = DateTime.Now.Year - EntreeEnFonction.Year;
+            if (DateTime.Now.DayOfYear < EntreeEnFonction.DayOfYear)
+                anciennete--; // Réduire l'ancienneté si l'anniversaire d'entrée n'est pas encore passé cette année
+            return anciennete;
+        }
+
+        public bool DroitStationnement() 
+        {
+            return Age() > 40 || Anciennete() > 20 ;
+           
+        }
+
+        public override string ToString()
+        {
+            return $"*** Données des employés *** \n" +
+                    $"Matricule: {Matricule}\n" +
+                    $"Nom: {Nom}\n" +
+                    $"Prénom: {Prenom} \n" +
+                    $"Date de naissance: {DateNaissance.ToString("yyyy/MM/dd")} \n" +
+                    $"Salaire annuel: {SalaireAnnuel:C} \n" +
+                    $"Date d'entrée en fonction: {EntreeEnFonction.ToString("yyyy/MM/dd")} \n" +
+                    $"Âge: {Age()} ans \n" +
+                    $"Anciennte: {Anciennete()} ans \n" +
+                    $"Droit au stationnement gratuit: {(DroitStationnement() ? "OUI" : "NON")}\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using RevisionEmploye;
+
+namespace RevisionEmploye
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+                try
+                {
+                    Employe employe = new Employe("3444645", "Daniela", "Quintana", new DateTime(1980, 11, 3), 50000, new DateTime(2000, 12, 1));
+                    Employe employe1 = new Employe("1000001", "Lemoine", "Pierre", new DateTime(1975, 3, 20), 60000, new DateTime(2005, 6, 1));
+                    Employe employe2 = new Employe("1000002", "Martin", "Claire", new DateTime(1985, 7, 15), 50000, new DateTime(2010, 9, 15));
+                    Employe employe3 = new Employe("1000003", "Robert", "Michel", new DateTime(1990, 11, 10), 75000, new DateTime(2015, 3, 1));
+                    Employe employe4 = new Employe("1000004", "Dupont", "Marie", new DateTime(1978, 5, 5), 65000, new DateTime(2000, 4, 25));
+                    Employe employe5 = new Employe("1000005", "Durand", "Jacques", new DateTime(1992, 9, 22), 72000, new DateTime(2012, 8, 15));
+
+                    List<Employe> employes = new List<Employe>();
+                    employes.Add(employe);
+                    employes.Add(employe1);
+                    employes.Add(employe2);
+                    employes.Add(employe3);
+                    employes.Add(employe4);
+                    employes.Add(employe5);
+                    
+ 
+
+                        for (int i = 0; i < employes.Count; i++)
+                        {
+                            Console.WriteLine(employes[i].ToString());
+                            Console.WriteLine("**************************");
+                            
+                        }
+                        Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erreur: " + ex.ToString ());
+
+                }
+
+        }
+    }
+}
